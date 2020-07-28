c47db94ceadb80ca18bcbfc49b1a49eda9781bd4
Salomon Memenza <salomemenza@gmail.com>
2020-07-28 11:55:41 -0500

    Initial Commit

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'salomemenza@gmail.com';
+  }
+
+
+}
